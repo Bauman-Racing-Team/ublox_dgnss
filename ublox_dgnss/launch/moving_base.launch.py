22924ea9f1fd3a496288bbdd3485ee6c0df98bbc
Markin Maxim <m_markin@baumanracing.ru>
2025-05-12 15:43:58 +0300

    [DRV-T-503] (*) Adapted u-blox driver launch interface

--- ublox_dgnss/launch/moving_base.launch.py
+++ ublox_dgnss/launch/moving_base.launch.py
@@ -10,27 +10,15 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    mb_config = get_package_share_directory("ublox_dgnss") + "/config" + "/moving_base_config.yaml"
-    rover_config = get_package_share_directory("ublox_dgnss") + "/config" + "/rover_config.yaml"
+    config = get_package_share_directory("ublox_dgnss") + "/config" + "/moving_base_config.yaml"
 
     ld = LaunchDescription(
         [
             Node(
-                parameters=[mb_config],
+                parameters=[config],
                 package="ublox_dgnss_node",
                 executable="ublox_dgnss_node",
                 name="ublox_moving_base",
-                namespace="brt8d",
-                output="screen",
-                # prefix=["gdbserver localhost:3000"],
-                # emulate_tty=True
-            ),
-            Node(
-                parameters=[rover_config],
-                package="ublox_dgnss_node",
-                executable="ublox_dgnss_node",
-                name="ublox_rover",
-                namespace="brt8d",
                 output="screen",
                 # prefix=["gdbserver localhost:3000"],
                 # emulate_tty=True
